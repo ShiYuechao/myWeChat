d9e85e49cb1ff477c8b38802cf285693397816a0
史越超 <946729069@qq.com>
2019-06-03 15:01:08 +0800

    push 初始化微信小程序仓库

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>这是index组件....</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/pages/index/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Index from './index.vue'
+
+const index = new Vue(Index);
+
+//挂载当前的页面
+index.$mount();
