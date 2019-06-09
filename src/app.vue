5246e766952462561cef476ec708d49b0639f7fd
史越超 <946729069@qq.com>
2019-06-09 22:49:44 +0800

    重置rem样式

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,18 @@
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
 /* 公共的样式 */
+html {
+  font-size: 62.5%; /* 10÷16=62.5% */
+}
+body {
+  font-size: 12px;
+  font-size: 1.2rem; /* 12÷10=1.2 */
+}
+p {
+  font-size: 14px;
+  font-size: 1.4rem;
+}
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,19 +1,30 @@
 <template>
-    <div>
-        <p>这是index组件....</p>
-    </div>
+  <div>
+    <header>
+        <p>这是index的标签</p>
+    </header>
+    <!-- <h1>这是index的标签</h1> -->
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            
-        }
-    },
-}
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-
+div > header {
+  width: 100%;
+  height: 3rem;
+  background: red;
+}
+div > header > p{
+    text-align: center;
+    font-size: 0.8rem;
+    line-height: 3rem;
+    margin: 0 auto;     
+}
 </style>
 
